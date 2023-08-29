3a355884ac67a1885e1998a493b24663678bc58c
gheorghe96 <curcagheorghe@gmail.com>
2023-08-29 11:21:14 +0300

    Add existing project files to Git

---
+++ pethealth/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  pethealth
+//
+//  Created by Gheorghe Curca on 29.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ pethealth/pethealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  pethealthApp.swift
+//  pethealth
+//
+//  Created by Gheorghe Curca on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct pethealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
